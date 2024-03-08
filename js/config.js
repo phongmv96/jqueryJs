5909ab52d57fa7b853e9a388abf95c369cf0482c
phong.mv <phongmv@ycomm.vn>
2024-03-08 14:38:49 +0700

    update head trigger deploy

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ const titleCard = 'Tặng Bảo Ngân';
 const contentCard = 'Chúc bngan 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
 const giftImage = 'img/bn.jpeg';
-const base64 = '';
+const base64 = ''
 const giftImageBase64 = "data:image/png;base64, " + base64;
 
