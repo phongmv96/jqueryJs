a579c056c990457523479130f05a8ac29b16b50f
phong.mv <phongmv@ycomm.vn>
2024-03-08 13:32:16 +0700

    update structure folder

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 const nameGirl = 'Bảo Ngân';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = 'https://www.facebook.com/duongbao.ngan.54?mibextid=ZbWKwL';
 const eventName = 'Chúc Mừng 8/3';
 const titleCard = 'Tặng Bảo Ngân';
 const contentCard = 'Chúc bngan 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
 
-const giftImage = 'bn.jpeg';
+const giftImage = 'img/bn.jpeg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
 
